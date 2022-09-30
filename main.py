5e12ba45abe305457b347a1dfed7a14365838a7f
Anna <chistozvonovaanna@gmail.com>
2022-09-30 16:30:29 +0300

    final project

--- main.py
+++ main.py
@@ -38,30 +38,10 @@ def get_questions(file_name):
         return questions
 
 
-# def test_get_questions():
-#     right_questions = [['Откуда обычно достает подарки Дед Мороз?', 'из шкатулки', 'из мешка', 'из-за пазухи',
-#                         'из интернет-магазина', '2'],
-#                        ['Что во время игры принимает волейболист?', 'подачу', 'роды', '100 грамм', 'снотворное', '1'],
-#                        ['Как же, согласно поговорке, там, где нас нет?', 'Тепло', 'Сытно', 'Хорошо', 'Весело', '3']]
-#     questions = get_questions("questions_test.txt")
-#     assert questions is not None
-#     if questions:
-#         for q in questions:
-#             assert q in right_questions
-#             questions.pop()
-
-
 def print_question(question):
     q = f"{question[0]}\nA. {question[1]}\nB. {question[2]}\nC. {question[3]}\nD. {question[4]}"
     print(q)
     return q
-
-
-# def test_print_question():
-#     question = ['Что во время игры принимает волейболист?', 'подачу', 'роды', '100 грамм', 'снотворное', '1']
-#     q = print_question(question)
-#     right_q = f"{question[0]}\nA. {question[1]}\nB. {question[2]}\nC. {question[3]}\nD. {question[4]}"
-#     assert q == right_q
 
 
 def print_half_question(question):
@@ -75,15 +55,6 @@ def print_half_question(question):
     q_print = f"{q[0]}\n{half_question[0][0]}. {half_question[0][1]}\n{half_question[1][0]}. {half_question[1][1]}"
     print(q_print)
     return q_print
-
-
-# def test_print_half_question():
-#     question = ['Что во время игры принимает волейболист?', 'подачу', 'роды', '100 грамм', 'снотворное', '1']
-#     q = print_half_question(question)
-#     right_q1 = f"{question[0]}\nA. {question[1]}\nB. {question[2]}"
-#     right_q2 = f"{question[0]}\nA. {question[1]}\nC. {question[3]}"
-#     right_q3 = f"{question[0]}\nA. {question[1]}\nD. {question[4]}"
-#     assert q == right_q1 or q == right_q2 or q == right_q3
 
 
 def command():
@@ -141,20 +112,6 @@ def play_round(num, point, questions_file):
             print("Верно.\n")
         else:
             print("Неверно.\n")
-
-
-# def test_play_round():
-#     str, answ = play_round(1, 100, "questions_test.txt")
-#     print (str, answ)
-#     questions = get_questions("questions_test.txt")
-#     right_round1 = f"Раунд 1. Каждый вопрос 100 баллов.\n"
-#     for question in questions:
-#         right_round1.join(f"{question[0]}\nA. {question[1]}\nB. {question[2]}\nC. {question[3]}\nD. {question[4]}")
-#     questions.reverse()
-#     right_round2 = f"Раунд 1. Каждый вопрос 100 баллов.\n"
-#     for question in questions:
-#         right_round2.join(f"{question[0]}\nA. {question[1]}\nB. {question[2]}\nC. {question[3]}\nD. {question[4]}")
-#     assert (str == right_round1 and answ == ["1", "3"]) or (str == right_round2 and answ == ["3", "1"])
 
 
 def play_game():
